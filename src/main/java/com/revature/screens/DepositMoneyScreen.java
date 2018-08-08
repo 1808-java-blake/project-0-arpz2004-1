3eb571fc70ee4afaa164d4f4ffe1e3e8745ffdda
Adrien Lindner <lindner.54@osu.edu>
2018-08-08 17:01:15 -0400

    Started withdraw and deposit screens

--- src/main/java/com/revature/screens/DepositMoneyScreen.java
+++ src/main/java/com/revature/screens/DepositMoneyScreen.java
@@ -1,37 +1,34 @@
 package com.revature.screens;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
+import com.revature.beans.BankAccount;
 import com.revature.beans.User;
-import com.revature.daos.UserDao;
+import com.revature.daos.BankAccountDao;
 
 public class DepositMoneyScreen implements Screen {
 	private Scanner scan = new Scanner(System.in);
-	private UserDao ud = UserDao.currentUserDao;
+	private BankAccountDao bad = BankAccountDao.currentBankAccountDao;
 
 	@Override
 	public Screen start() {
-		User u = new User();
-		System.out.println("Enter new username");
-		u.setUsername(scan.nextLine());
-		System.out.println("Enter password");
-		u.setPassword(scan.nextLine());
-		System.out.println("Enter first name");
-		u.setFirstName(scan.nextLine());
-		System.out.println("Enter last name");
-		u.setLastName(scan.nextLine());
-		System.out.println("Enter age");
-		String age = scan.nextLine();
-		
-		try {
-			u.setAge(Integer.valueOf(age));
-			ud.createUser(u);
-			
-		} catch (NumberFormatException e) {
-			System.out.println("Invalid number");
-		}
-		
-		return new LoginScreen();
+		String selection;
+		boolean validSelection = false;
+		do {
+			System.out.println("Enter the amount you want to deposit:");
+			selection = scan.nextLine();
+			validSelection = true;
+			if(!validSelection) {
+				System.out.println("Invalid option selected.");
+			}
+		} while (!validSelection);
+		BankAccount ba = bad.findByUsername(User.getCurrentUser().getUsername());
+		ba.setBalance(ba.getBalance().add(new BigDecimal(Integer.parseInt(selection))));
+		System.out.println("Your new bank account balance is $" + ba.getBalance() + ".");
+		System.out.println("Press enter to return to home screen.");
+		scan.nextLine();
+		return new HomeScreen();
 	}
 
 }
